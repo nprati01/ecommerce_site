c65b2e2a98160e6edd72b78d7cd5a8b55e092972
nprati01 <nicole.e.prati@gmail.com>
2023-06-30 17:41:11 -0400

    add category and product model

---
+++ main_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 250, db_index=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    brand = models.CharField(max_length=250, default='un-branded')
+    # blank= true makes this an optional field when creating a product
+    description = models.TextField(blank=True)
+    # makes unique product
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.title
